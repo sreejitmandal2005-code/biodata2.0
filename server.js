3a6fac7389fabe3ee698e9cc43c8c4ef3e1345dd
sreejitmandal2005-code <sreejitmandal2005@gmail.com>
2026-02-21 08:09:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = 3000;
@@ -8,13 +9,38 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Gmail Transport Setup
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "sreejitmandal2005@gmail.com",       // 🔥 YOUR GMAIL
+        pass: "bhagyashri2005sreejit2007"     // 🔥 16 digit app password
+    }
+});
+
 app.post("/contact", (req, res) => {
     const { name, email, message } = req.body;
-    console.log("New Message:");
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Message:", message);
-    res.send("<h2>Message Sent Successfully 🚀</h2><a href='/'>Go Back</a>");
+
+    const mailOptions = {
+        from: email,
+        to: "sreejitmandal2005@gmail.com",  // 🔥 Where you want to receive messages
+        subject: `New Message from ${name}`,
+        text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+        `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send("Error sending message ❌");
+        } else {
+            console.log("Email sent: " + info.response);
+            res.send("<h2>Message Sent Successfully 🚀</h2><a href='/'>Go Back</a>");
+        }
+    });
 });
 
 app.listen(PORT, () => {
